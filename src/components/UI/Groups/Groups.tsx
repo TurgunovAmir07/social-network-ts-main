538e7b1cf19f47b3453919a62d8091096723901d
Amir <turgunovamir9@gmail.com>
2023-12-11 15:32:52 +0500

    none

---
+++ src/components/UI/Groups/Groups.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+export const Group = () => {
+  return (
+    <>
+      <div className="UserElem">
+        <img src="./img/profile/profile-img-1.jpeg" alt="User" />
+        <div className="user__description">
+          <p className="main__text">N E W</p>
+          <p className="secondary__text _online">Развитие</p>
+        </div>
+        <span className="Badge">3</span>
+      </div>
+      <div className="UserElem">
+        <img src="./img/profile/profile-img-2.jpeg" alt="User" />
+        <div className="user__description">
+          <p className="main__text">Aesthetics</p>
+          <p className="secondary__text _online">Стиль</p>
+        </div>
+        <span className="Badge">3</span>
+      </div>
+      <div className="UserElem">
+        <img src="./img/profile/profile-img-3.jpeg" alt="User" />
+        <div className="user__description">
+          <p className="main__text">дом твоей эстетики</p>
+          <p className="secondary__text _online">Творчество</p>
+        </div>
+        <span className="Badge">3</span>
+      </div>
+      <div className="UserElem">
+        <img src="./img/profile/profile-img-4.jpeg" alt="User" />
+        <div className="user__description">
+          <p className="main__text">wailet</p>
+          <p className="secondary__text _online">Искусство</p>
+        </div>
+        <span className="Badge">3</span>
+      </div>
+      <div className="UserElem">
+        <img src="./img/profile/profile-img-5.jpeg" alt="User" />
+        <div className="user__description">
+          <p className="main__text">A W E S O M E</p>
+          <p className="secondary__text _online">Стиль</p>
+        </div>
+        <span className="Badge">3</span>
+      </div>
+      <div className="UserElem">
+        <img src="./img/profile/profile-img-6.jpeg" alt="User" />
+        <div className="user__description">
+          <p className="main__text">minimalism</p>
+          <p className="secondary__text _online">Фотография</p>
+        </div>
+        <span className="Badge">3</span>
+      </div>
+      <div className="UserElem">
+        <img src="./img/profile/profile-img-7.jpeg" alt="User" />
+        <div className="user__description">
+          <p className="main__text">Словарный запас</p>
+          <p className="secondary__text _online">Литература</p>
+        </div>
+        <span className="Badge">3</span>
+      </div>
+      <div className="UserElem">
+        <img src="./img/profile/profile-img-8.jpeg" alt="User" />
+        <div className="user__description">
+          <p className="main__text">Look</p>
+          <p className="secondary__text _online">Мода</p>
+        </div>
+        <span className="Badge">3</span>
+      </div>
+    </>
+  );
+};
---
+++ src/components/UI/User/UserElem.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const UserElem = () => {
+  return (
+    <>
+      <div className="UserElem">
+        <img src="./img/users/aleksandr-maykov.jpeg" alt="User" />
+        <div className="user__description">
+          <p className="main__text">Александр Майков</p>
+          <p className="secondary__text _online">Онлайн</p>
+        </div>
+        <span className="Badge">3</span>
+      </div>
+      <div className="UserElem">
+        <img src="./img/users/aleksandr-maykov.jpeg" alt="User" />
+        <div className="user__description">
+          <p className="main__text">Александр Майков</p>
+          <p className="secondary__text _online">Онлайн</p>
+        </div>
+        <span className="Badge">3</span>
+      </div>
+      <div className="UserElem">
+        <img src="./img/users/aleksandr-maykov.jpeg" alt="User" />
+        <div className="user__description">
+          <p className="main__text">Александр Майков</p>
+          <p className="secondary__text _online">Онлайн</p>
+        </div>
+        <span className="Badge">3</span>
+      </div>
+    </>
+  );
+};
